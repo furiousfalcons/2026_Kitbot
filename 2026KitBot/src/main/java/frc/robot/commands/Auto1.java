b1b5b663d109883f58a1108fb02f8a4193881ba0
santiago <santiagohmarroquin@gmail.com>
2026-03-18 18:27:38 -0500

    end of meeting changes

--- 2026KitBot/src/main/java/frc/robot/commands/Auto1.java
+++ 2026KitBot/src/main/java/frc/robot/commands/Auto1.java
@@ -3,11 +3,15 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.CANDriveSubsystem;
 import frc.robot.subsystems.CANFuelSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
 
 public class Auto1 extends SequentialCommandGroup {
 
     public Auto1(CANDriveSubsystem driveSubsystem, CANFuelSubsystem ballSubsystem, double angle1, double angle2, double distance, double speed){
-        addCommands(new TurnToAngle(driveSubsystem, angle1).withTimeout(2), //turn to straight path toward depot
+        addCommands(new DriveDistance(driveSubsystem, false, 0.5).withTimeout(0.5), // move back to shoot
+        new Launch(ballSubsystem).withTimeout(7), // shoooot
+        new DriveDistance(driveSubsystem, true, 0.5).withTimeout(0.5), // return to start
+        new TurnToAngle(driveSubsystem, angle1).withTimeout(2), //turn to straight path toward depot
         new DriveDistance(driveSubsystem, false, speed).withTimeout(distance/ speed), //straight path to depot
         new TurnToAngle(driveSubsystem, angle2).withTimeout(2), //align parallely with depot
         new IntakeAndReverse(ballSubsystem, driveSubsystem).withTimeout(4), // intaking while backing into the depot
--- 2026KitBot/src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ 2026KitBot/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -84,4 +84,6 @@ public class VisionSubsystem extends SubsystemBase {
 
     }
 
+    
+
 }
